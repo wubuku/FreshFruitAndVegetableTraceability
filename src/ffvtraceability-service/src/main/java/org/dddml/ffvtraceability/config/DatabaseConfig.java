ac84091159c4375a5f92591145f62e2bebe0bf99
yangjiefeng <yangjiefeng@gmail.com>
2024-12-18 22:13:20 +0800

    update DatabaseConfig

--- src/ffvtraceability-service/src/main/java/org/dddml/ffvtraceability/config/DatabaseConfig.java
+++ src/ffvtraceability-service/src/main/java/org/dddml/ffvtraceability/config/DatabaseConfig.java
@@ -3,33 +3,42 @@ package org.dddml.ffvtraceability.config;
 import org.dddml.ffvtraceability.domain.hibernate.TenantInterceptor;
 import org.dddml.ffvtraceability.specialization.NullReadOnlyProxyGenerator;
 import org.dddml.ffvtraceability.specialization.ReadOnlyProxyGenerator;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
+import org.hibernate.cfg.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.core.io.support.ResourcePatternUtils;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
-import org.hibernate.cfg.Environment;
-import org.hibernate.SessionFactory;
-import jakarta.persistence.EntityManagerFactory;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 import javax.sql.DataSource;
 import java.io.IOException;
-import java.util.Properties;
 import java.util.Arrays;
+import java.util.Properties;
 
 // Enables Spring's annotation-driven transaction management capability, similar to the support found in Spring's <tx:*> XML namespace.
 @EnableTransactionManagement
 @org.springframework.context.annotation.Configuration
 public class DatabaseConfig {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private ResourceLoader resourceLoader;
 
     @Autowired
     private DataSource dataSource;
+
+    // 注入 Spring 配置
+    @Autowired
+    private org.springframework.core.env.Environment springEnvironment;
+
+    @Value("${spring.jpa.properties.hibernate.dialect}")
+    private String hibernateDialect;
 
     @Bean(name = {
             "hibernateSessionFactory",
@@ -40,7 +49,7 @@ public class DatabaseConfig {
 
         // Load HBM XML mappings
         Arrays.stream(ResourcePatternUtils.getResourcePatternResolver(resourceLoader)
-                .getResources("classpath:/hibernate/*.hbm.xml"))
+                        .getResources("classpath:/hibernate/*.hbm.xml"))
                 .forEach(resource -> {
                     try {
                         configuration.addInputStream(resource.getInputStream());
@@ -52,29 +61,29 @@ public class DatabaseConfig {
 
         // Scan for annotated entities
         configuration.addPackage("org.dddml.ffvtraceability.*");
-
         configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
 
-        Properties hibernateProperties = Environment.getProperties();
+        // Read config from application-test.yml
+        Properties hibernateProperties = new Properties();
+
+        hibernateProperties.setProperty("hibernate.dialect", hibernateDialect);
+        hibernateProperties.setProperty("hibernate.hbm2ddl.auto",
+                springEnvironment.getProperty("spring.jpa.properties.hibernate.hbm2ddl.auto"));
+        hibernateProperties.setProperty("hibernate.show_sql",
+                springEnvironment.getProperty("spring.jpa.show-sql", "true"));
+        hibernateProperties.setProperty("hibernate.connection.pool_size",
+                springEnvironment.getProperty("spring.jpa.properties.hibernate.connection.pool_size", "1"));
+        hibernateProperties.setProperty("hibernate.cache.provider_class",
+                springEnvironment.getProperty("spring.jpa.properties.hibernate.cache.provider_class"));
+        hibernateProperties.setProperty("hibernate.cache.use_second_level_cache",
+                springEnvironment.getProperty("spring.jpa.properties.hibernate.cache.use_second_level_cache"));
+
         hibernateProperties.put(Environment.INTERCEPTOR, entityInterceptor());
         hibernateProperties.put(Environment.DATASOURCE, dataSource);
         configuration.setProperties(hibernateProperties);
 
         return configuration.buildSessionFactory();
     }
-
-    // @Bean
-    // public EntityManagerFactory entityManagerFactory(SessionFactory sessionFactory) {
-    //     LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-    //     em.setDataSource(dataSource);
-    //     em.setPackagesToScan("org.dddml.ffvtraceability.*");
-    //     em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-    //     Properties jpaProperties = new Properties();
-    //     jpaProperties.put("hibernate.session_factory", sessionFactory);
-    //     em.setJpaProperties(jpaProperties);
-    //     em.afterPropertiesSet();
-    //     return em.getObject();
-    // }
 
     @Bean
     public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
