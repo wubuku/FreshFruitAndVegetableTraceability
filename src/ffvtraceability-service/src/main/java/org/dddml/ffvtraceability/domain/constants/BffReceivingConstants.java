e664b4da5082c0d4bb0a748e972b5c2a5886f047
yangjiefeng <yangjiefeng@gmail.com>
2025-02-18 13:32:40 +0800

    update constants

---
+++ src/ffvtraceability-service/src/main/java/org/dddml/ffvtraceability/domain/constants/BffReceivingConstants.java
@@ -0,0 +1,19 @@
+package org.dddml.ffvtraceability.domain.constants;
+
+public class BffReceivingConstants {
+    private BffReceivingConstants() {
+        // Prevent instantiation
+    }
+
+
+    /**
+     * Receiving Reference Document
+     */
+    public static final String DOCUMENT_TYPE_RECV_REF_DOC = "RECV_REF_DOC";
+
+    /**
+     * Receiving QA Inspection Document
+     */
+    public static final String DOCUMENT_TYPE_RECV_QA_INSP_DOC = "RECV_QA_INSP_DOC";
+
+}
