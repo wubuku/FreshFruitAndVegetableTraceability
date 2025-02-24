d6204ca77da8d97af4a7b81995fc7a07a0365bf6
yangjiefeng <yangjiefeng@gmail.com>
2025-02-24 10:04:02 +0800

    update file service CORS config

---
+++ src/ffvtraceability-file-service/src/main/java/org/dddml/ffvtraceability/fileservice/config/WebConfig.java
@@ -0,0 +1,31 @@
+package org.dddml.ffvtraceability.fileservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${spring.mvc.cors.allowed-origins:*}")
+    private String allowedOrigins;
+
+    @Value("${spring.mvc.cors.allowed-methods:*}")
+    private String allowedMethods;
+
+    @Value("${spring.mvc.cors.allowed-headers:*}")
+    private String allowedHeaders;
+
+    @Value("${spring.mvc.cors.exposed-headers:*}")
+    private String exposedHeaders;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods(allowedMethods)
+                .allowedHeaders(allowedHeaders)
+                .exposedHeaders(exposedHeaders);
+    }
+} 
