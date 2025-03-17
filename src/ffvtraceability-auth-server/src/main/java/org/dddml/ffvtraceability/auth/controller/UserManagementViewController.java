c92c523ea3ca580d2966f9c77134ac9369051362
whilliy <return@126.com>
2025-03-17 21:22:07 +0800

    update

--- src/ffvtraceability-auth-server/src/main/java/org/dddml/ffvtraceability/auth/controller/UserManagementViewController.java
+++ src/ffvtraceability-auth-server/src/main/java/org/dddml/ffvtraceability/auth/controller/UserManagementViewController.java
@@ -5,8 +5,10 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/auth-srv")
 public class UserManagementViewController {
 
     @GetMapping("/user-management")
