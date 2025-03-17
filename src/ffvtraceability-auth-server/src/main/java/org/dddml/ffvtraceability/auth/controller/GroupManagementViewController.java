a84c9d68ced8f8b83fd536096fb5f19a91bf9b46
whilliy <return@126.com>
2025-03-17 19:10:27 +0800

    update

--- src/ffvtraceability-auth-server/src/main/java/org/dddml/ffvtraceability/auth/controller/GroupManagementViewController.java
+++ src/ffvtraceability-auth-server/src/main/java/org/dddml/ffvtraceability/auth/controller/GroupManagementViewController.java
@@ -3,8 +3,10 @@ package org.dddml.ffvtraceability.auth.controller;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/auth-srv")
 public class GroupManagementViewController {
 
     @GetMapping("/group-management")
