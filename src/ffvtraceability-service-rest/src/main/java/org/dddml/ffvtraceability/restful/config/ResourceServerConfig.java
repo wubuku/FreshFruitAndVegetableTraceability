c998abe3e46a46ad90ab0d5cc5e10594037c89e7
whilliy <return@126.com>
2025-03-17 15:29:30 +0800

    ,https://fp.ablueforce.com 放行

--- src/ffvtraceability-service-rest/src/main/java/org/dddml/ffvtraceability/restful/config/ResourceServerConfig.java
+++ src/ffvtraceability-service-rest/src/main/java/org/dddml/ffvtraceability/restful/config/ResourceServerConfig.java
@@ -54,7 +54,7 @@ public class ResourceServerConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList(
-                "http://localhost:9000","https://demo-git-master-liugan2222s-projects.vercel.app/"
+                "http://localhost:9000",",https://fp.ablueforce.com","https://demo-git-master-liugan2222s-projects.vercel.app/"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList(
