9fdebe7edebaa138cfbf63a3269167a429481603
yangjiefeng <yangjiefeng@gmail.com>
2025-04-13 10:51:59 +0800

    update

--- src/ffvtraceability-common/src/generated/java/org/dddml/ffvtraceability/domain/inventoryitem/AbstractInventoryItemState.java
+++ src/ffvtraceability-common/src/generated/java/org/dddml/ffvtraceability/domain/inventoryitem/AbstractInventoryItemState.java
@@ -757,11 +757,16 @@ public abstract class AbstractInventoryItemState implements InventoryItemState.S
         this.setUpdatedBy(e.getCreatedBy());
         this.setUpdatedAt(e.getCreatedAt());
 
+        ApplicationContext.current.setRequesterId(e.getCreatedBy());
+        try {
         InventoryItemState updatedInventoryItemState = ApplicationContext.current.get(IRecordInventoryEntryLogic.class).mutate(
                 this, inventoryItemAttributes, inventoryItemDetailAttributes, quantityOnHandDiff, availableToPromiseDiff, accountingQuantityDiff, unitCost, MutationContext.of(e, s -> {if (s == this) {return this;} else {throw new UnsupportedOperationException("Current MutationContext implementation only supports returning the same state instance");}}));
 
 
         if (this != updatedInventoryItemState) { merge(updatedInventoryItemState); } //else do nothing
+        } finally {
+            ApplicationContext.current.clearRequesterId();
+        }
 
     }
 
@@ -773,6 +778,8 @@ public abstract class AbstractInventoryItemState implements InventoryItemState.S
         java.math.BigDecimal accountingQuantityDiff = e.getAccountingQuantityDiff();
         java.math.BigDecimal unitCost = e.getUnitCost();
 
+        ApplicationContext.current.setRequesterId(e.getCreatedBy());
+        try {
         AbstractInventoryItemState _this = (AbstractInventoryItemState) ApplicationContext.current.get(IRecordInventoryEntryLogic.class).mutate(
                 null, inventoryItemAttributes, inventoryItemDetailAttributes, quantityOnHandDiff, availableToPromiseDiff, accountingQuantityDiff, unitCost, MutationContext.forCreation(e, id -> stateFactory.apply((String) id) ));
 
@@ -789,6 +796,9 @@ public abstract class AbstractInventoryItemState implements InventoryItemState.S
         _this.setUpdatedAt(e.getCreatedAt());
 
         return _this;
+        } finally {
+            ApplicationContext.current.clearRequesterId();
+        }
     }
 
     public void save() {
